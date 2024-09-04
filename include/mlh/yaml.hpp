bad19080d20b02e5fad2fbb5b8157cbedd8a2260
Maximilian Leo Huber <huber.maximilian.leo@gmail.com>
2024-09-04 21:35:32 +0200

    Basic structure of parsing yaml

--- include/mlh/yaml.hpp
+++ include/mlh/yaml.hpp
@@ -16,7 +16,7 @@
 
 namespace mlh {
 
-// reference: https://yaml.org/refcard.html
+// Reference: https://yaml.org/refcard.html
 // %YAML 1.1   # Reference card
 enum class YamlTokens {
   //
@@ -198,18 +198,72 @@ enum class YamlTokens {
 
   // { "\e": ESC, "\_": NBSP, "\N": NEL, "\L": LS, "\P": PS }
   Additional,
+
+  //
+  // Additional tokens:
+  //
+
+  // '#'  : Comment, that starts with the # character anywhere 
+  //        on a line and ends with the end of the line
+  Comment,
+
+  // ' '  : If at the start of a line and part of a list, it is an indentation
+  //        (TODO: double check when whitespaces count as meaningful indentation)
+  WhiteSpaceIndentation,
+
+  // '\t'  : Tab characters are not allowed as part of whitespace indentation.
+  //         Tab only indentation should be fine (TODO: double check)
+  TabIndentation,
+};
+
+enum class ASTElements {
+  // TODO:
+};
+
+class YamlAST {
+public:
+  YamlAST(std::vector<YamlTokens>);
+  bool isValid();
+  void print();
+private:
+  // TODO: implement Abstract Syntax Tree
 };
 
 class Yaml {
 public:
   Yaml() = default;
 
-  static Yaml parse(std::ifstream &InputStream) {
+  static Yaml parse(std::ifstream &InputStream, bool PrintASTs = false) {
     assert(InputStream.is_open());
 
-    Yaml NewYaml = Yaml();
     std::string Container = "";
 
+    readStream(InputStream, Container);
+
+    bool Valid = parseString(Container, PrintASTs);
+
+    if (!Valid) {
+      std::cerr << "YAML file is not valid!" <<  std::endl;
+    }
+
+    Yaml NewYaml = Yaml();
+    NewYaml.setContent(Container);
+
+    return NewYaml;
+  }
+
+  inline void setContent(const std::string &Str) { Content = Str; }
+
+  inline std::string getContent() { return Content; }
+
+private:
+  std::string Content = "";
+ 
+  //
+  // Functions for parsing
+  //
+
+  static void readStream(std::ifstream &InputStream, std::string &Container) {
     while (true) {
       const char CurrChar = InputStream.get();
 
@@ -219,41 +273,83 @@ public:
 
       Container += CurrChar;
     }
+  }
 
-    if (validateString(Container)) {
-      NewYaml.setContent(Container);
-    } else {
-      std::cerr << "YAML file is not valid!" << std::endl;
+  static size_t getNumberOfDocumentHeaders(const std::string &Str) {
+    size_t NumberOfDocumentHeaders = 0;
+    size_t MinusCounter = 0;
+
+    for (const char CurrChar : Str) {
+      if (CurrChar == '-') {
+        MinusCounter++;
+      }
+      if (MinusCounter == 2) {
+        NumberOfDocumentHeaders++;
+        MinusCounter = 0;
+      }
     }
 
-    return NewYaml;
+    return NumberOfDocumentHeaders;
   }
 
-  static bool validateString(const std::string &Str) {
-    tokenizeString(Str);
+  static void getIndicesOfNewDocuments(const std::string &Str, std::vector<size_t> &Container) {
+    size_t MinusCounter = 0;
+    size_t Index = 0;
 
-    bool Valid = checkTokens();
-    Tokens.clear();
+    for (const char CurrChar : Str) {
+      if (CurrChar == '-') {
+        MinusCounter++;
+      }
+      if (MinusCounter == 2) {
+        MinusCounter = 0;
+        Container.push_back(Index - 2);
+      }
+
+      Index++;
+    }
+  }
+
+  static void splitStringByDocumentHeaders(const std::string &Str, std::vector<std::string> &Container) {
+    std::vector<size_t> DocumentHeaderIndices;
+    getIndicesOfNewDocuments(Str, DocumentHeaderIndices);
+
+    size_t Index = 0;
+    size_t DocumentIndex = 0;
+    for (char CurrChar : Str) {
+      Container[DocumentIndex] += CurrChar;
+
+      Index++;
+      if (Index == DocumentHeaderIndices[DocumentIndex]) {
+        DocumentIndex++;
+      }
+    }
+  }
+
+  static void tokenizeDocument(const std::string &Str, std::vector<YamlTokens> &TokenContainer) {
+
+  }
+
+  static bool parseString(const std::string &Str, bool PrintASTs) {
+    std::vector<std::string> Documents(getNumberOfDocumentHeaders(Str));
+    splitStringByDocumentHeaders(Str, Documents);
+
+    bool Valid = true;
+
+    for (const auto &Doc : Documents) {
+      std::vector<YamlTokens> Tokens;
+      tokenizeDocument(Doc, Tokens);
+
+      YamlAST CurrDocAST(Tokens);
+      if (!CurrDocAST.isValid()) {
+        Valid = false;
+      }
+
+      if (PrintASTs) {
+        CurrDocAST.print();
+      }
+    }
 
     return Valid;
-  }
-
-  inline void setContent(const std::string &Str) { Content = Str; }
-
-  inline std::string getContent() { return Content; }
-
-private:
-  std::string Content = "";
-
-  static std::vector<YamlTokens> Tokens;
-
-  static void tokenizeString(const std::string &Str) {
-    // TODO:
-  }
-
-  static bool checkTokens() {
-    // TODO:
-    return false;
   }
 };
 
